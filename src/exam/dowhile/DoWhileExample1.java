5bea8b8dfcd3e726c98e5c9270d670615d00a37e
Mute <zzzqw59@gmail.com>
2026-05-07 12:58:33 +0900

    do while 1

---
+++ src/exam/dowhile/DoWhileExample1.java
@@ -0,0 +1,14 @@
+package exam.dowhile;
+
+public class DoWhileExample1 {
+    static void main(String[] args) {
+        char alphabet = 'a';
+
+        do {
+            System.out.println(alphabet);
+            alphabet++;
+        } while(alphabet < 123);
+
+
+    }
+}
